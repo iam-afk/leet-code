ff066b873ce053baebeb36b4ce52eaae7466c06b
Andrey Manzharov <andreyrmg@gmail.com>
2022-09-01 20:20:37 +0300

    ✅ 1448. Count Good Nodes in Binary Tree
    Runtime: 42 ms
    Memory: 6.9 MB

--- src/lib.rs
+++ src/lib.rs
@@ -9,5 +9,44 @@ mod p0387;
 mod p0804;
 mod p1329;
 mod p1338;
+mod p1448;
 
 pub struct Solution;
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val,
+            left: None,
+            right: None,
+        }
+    }
+
+    pub fn from(vec: Vec<Option<i32>>) -> Option<Rc<RefCell<TreeNode>>> {
+        Self::node(0, vec.as_slice())
+    }
+
+    fn node(index: usize, vec: &[Option<i32>]) -> Option<Rc<RefCell<TreeNode>>> {
+        if index < vec.len() {
+            if let Some(val) = vec[index] {
+                return Some(Rc::new(RefCell::new(TreeNode {
+                    val,
+                    left: Self::node(index * 2 + 1, vec),
+                    right: Self::node(index * 2 + 2, vec),
+                })));
+            }
+        }
+        None
+    }
+}
---
+++ src/p1448.rs
@@ -0,0 +1,53 @@
+use crate::{Solution, TreeNode};
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+impl Solution {
+    pub fn good_nodes(root: Option<Rc<RefCell<TreeNode>>>) -> i32 {
+        Self::good_nodes_0(root, i32::MIN)
+    }
+    fn good_nodes_0(root: Option<Rc<RefCell<TreeNode>>>, maximum: i32) -> i32 {
+        if let Some(node) = root {
+            let node = node.borrow();
+            let current = if node.val >= maximum { 1 } else { 0 };
+            current
+                + Self::good_nodes_0(node.left.clone(), node.val.max(maximum))
+                + Self::good_nodes_0(node.right.clone(), node.val.max(maximum))
+        } else {
+            0
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example() {
+        assert_eq!(
+            Solution::good_nodes(TreeNode::from(vec![
+                Some(3),
+                Some(1),
+                Some(4),
+                Some(3),
+                None,
+                Some(1),
+                Some(5)
+            ])),
+            4
+        );
+        assert_eq!(
+            Solution::good_nodes(TreeNode::from(vec![
+                Some(3),
+                Some(3),
+                None,
+                Some(4),
+                Some(2)
+            ])),
+            3
+        );
+        assert_eq!(Solution::good_nodes(TreeNode::from(vec![Some(1)])), 1);
+    }
+}
