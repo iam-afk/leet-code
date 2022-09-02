dd62ef2374c31ebb7364fdfe2cb999b8e2cdd11e
Andrey Manzharov <andreyrmg@gmail.com>
2022-09-02 11:33:57 +0300

    ✅ 637. Average of Levels in Binary Tree
    Runtime: 4 ms
    Memory: 2.9 MB

---
+++ src/p0637.rs
@@ -0,0 +1,61 @@
+use crate::Solution;
+use crate::TreeNode;
+
+use std::cell::RefCell;
+use std::rc::Rc;
+
+impl Solution {
+    pub fn average_of_levels(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<f64> {
+        let mut result = vec![];
+        let mut level = vec![root.clone()];
+        while !level.is_empty() {
+            let count = level.len();
+            let mut sum = 0i64;
+            let mut next_level = vec![];
+            for node in level.into_iter().flatten() {
+                let node = node.borrow();
+                sum += node.val as i64;
+                if node.left.is_some() {
+                    next_level.push(node.left.clone());
+                }
+                if node.right.is_some() {
+                    next_level.push(node.right.clone());
+                }
+            }
+            result.push(sum as f64 / count as f64);
+            level = next_level;
+        }
+        result
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn example() {
+        assert_eq!(
+            Solution::average_of_levels(TreeNode::from(vec![
+                Some(3),
+                Some(9),
+                Some(20),
+                None,
+                None,
+                Some(15),
+                Some(7)
+            ])),
+            [3.00000, 14.50000, 11.00000]
+        );
+        assert_eq!(
+            Solution::average_of_levels(TreeNode::from(vec![
+                Some(3),
+                Some(9),
+                Some(20),
+                Some(15),
+                Some(7)
+            ])),
+            [3.00000, 14.50000, 11.00000]
+        );
+    }
+}
