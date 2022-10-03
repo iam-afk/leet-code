6870b7e1f718cf8792f3e581ee53b357856c75aa
Andrey Manzharov <amanzharov@yandex.ru>
2022-10-03 15:13:00 +0300

    ✅ 1578. Minimum Time to Make Rope Colorful (Two pointers)
    Runtime: 14 ms
    Memory: 3.2 MB

--- src/p1578.rs
+++ src/p1578.rs
@@ -1,31 +1,25 @@
-use std::cmp::min;
-
 use crate::Solution;
 
 impl Solution {
     pub fn min_cost(colors: String, needed_time: Vec<i32>) -> i32 {
-        let mut d = vec![[-1; 26 + 1]; colors.len() + 1];
-        Self::find(colors.as_bytes(), &needed_time, b'z' + 1, &mut d)
-    }
-
-    fn find(colors: &[u8], needed_time: &[i32], last: u8, d: &mut Vec<[i32; 27]>) -> i32 {
-        let x = d[colors.len()][(last - b'a') as usize];
-        if x >= 0 {
-            return x;
-        }
-        let x = match (colors, needed_time) {
-            ([], []) => 0,
-            ([c, rest_colors @ ..], [t, rest_time @ ..]) if *c == last => {
-                Self::find(rest_colors, rest_time, last, d) + t
+        let colors = colors.as_bytes();
+        let mut p = 0u8;
+        let mut sum = 0;
+        let mut max = 0;
+        let mut result = 0;
+        for (i, c) in colors.iter().enumerate() {
+            if colors[i] != p {
+                result += sum - max;
+                sum = needed_time[i];
+                max = sum;
+            } else {
+                sum += needed_time[i];
+                max = needed_time[i].max(max);
             }
-            ([c, rest_colors @ ..], [t, rest_time @ ..]) => min(
-                Self::find(rest_colors, rest_time, last, d) + t,
-                Self::find(rest_colors, rest_time, *c, d),
-            ),
-            _ => unreachable!(),
-        };
-        d[colors.len()][(last - b'a') as usize] = x;
-        x
+            p = *c;
+        }
+        result += sum - max;
+        result
     }
 }
 
