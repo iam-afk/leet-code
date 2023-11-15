786f57a89f3168751c35792f1b83ae4022bd7800
Andrey Manzharov <amanzharov@yandex.ru>
2023-11-15 09:47:24 +0300

    ☝️ 1846. Maximum Element After Decreasing and Rearranging

--- 1846.maximum-element-after-decreasing-and-rearranging-c.c
+++ 1846.maximum-element-after-decreasing-and-rearranging-c.c
@@ -1,20 +1,18 @@
-static int
-compare_ints(void const* a, void const* b)
-{
-  return *(int const*)a - *(int const*)b;
-}
+#define min(a, b)                                                                                  \
+  ({                                                                                               \
+    __typeof__(a) _a = (a);                                                                        \
+    __typeof__(b) _b = (b);                                                                        \
+    _a < _b ? _a : _b;                                                                             \
+  })
 
 int
 maximumElementAfterDecrementingAndRearranging(int* arr, int arr_size)
 {
+  int count[100001] = { 0 };
+  for (int i = 0; i < arr_size; ++i)
+    ++count[min(arr_size, arr[i])];
   int maximum = 1;
-  qsort(arr, arr_size, sizeof(int), compare_ints);
-  arr[0] = 1;
-  for (int i = 1; i < arr_size; ++i) {
-    if (arr[i - 1] + 1 < arr[i])
-      arr[i] = arr[i - 1] + 1;
-    if (arr[i] > maximum)
-      maximum = arr[i];
-  }
+  for (int i = 2; i <= arr_size; ++i)
+    maximum = min(i, maximum + count[i]);
   return maximum;
 }
