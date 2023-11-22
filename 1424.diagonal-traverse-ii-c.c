166a9fbb9f74d28fead9fbb96be6ef37747ecc0e
Andrey Manzharov <amanzharov@yandex.ru>
2023-11-22 10:29:13 +0300

    ☝️ 1424. Diagonal Traverse II

--- 1424.diagonal-traverse-ii-c.c
+++ 1424.diagonal-traverse-ii-c.c
@@ -1,19 +1,3 @@
-
-typedef struct
-{
-  int i, j;
-} pos_t;
-
-static int
-compare_pos(void const* a, void const* b)
-{
-  pos_t const *pos_a = (pos_t*)a, *pos_b = (pos_t*)b;
-  int k = pos_a->i + pos_a->j - pos_b->i - pos_b->j;
-  if (k != 0)
-    return k;
-  return pos_b->i - pos_a->i;
-}
-
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
@@ -21,18 +5,37 @@ int*
 findDiagonalOrder(int** nums, int nums_size, int* nums_col_size, int* return_size)
 {
   *return_size = 0;
-
-  pos_t index[100000];
   for (int i = 0; i < nums_size; ++i)
-    for (int j = 0; j < nums_col_size[i]; ++j)
-      index[(*return_size)++] = (pos_t){ .i = i, .j = j };
-
-  qsort(index, *return_size, sizeof(pos_t), compare_pos);
+    *return_size += nums_col_size[i];
 
   int* answer = calloc(*return_size, sizeof *answer);
+  int last = 0;
 
-  for (int i = 0; i < *return_size; ++i)
-    answer[i] = nums[index[i].i][index[i].j];
+  typedef struct
+  {
+    int i, j;
+  } pos_t;
+  pos_t queue[nums_size + 1];
+  int front = 0, tail = 0, len = 0;
+
+  queue[tail] = (pos_t){ .i = 0, .j = 0 };
+  tail = (tail + 1) % (nums_size + 1), ++len;
+
+  while (len > 0) {
+    pos_t p = queue[front];
+    front = (front + 1) % (nums_size + 1), --len;
+
+    answer[last++] = nums[p.i][p.j];
+
+    if (p.j == 0 && p.i + 1 < nums_size) {
+      queue[tail] = (pos_t){ .i = p.i + 1, .j = 0 };
+      tail = (tail + 1) % (nums_size + 1), ++len;
+    }
+    if (p.j + 1 < nums_col_size[p.i]) {
+      queue[tail] = (pos_t){ .i = p.i, .j = p.j + 1 };
+      tail = (tail + 1) % (nums_size + 1), ++len;
+    }
+  }
 
   return answer;
 }
