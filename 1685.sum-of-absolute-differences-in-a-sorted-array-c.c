b97f9654e5388e46ed30d66aa1da1aa0c60872ca
Andrey Manzharov <amanzharov@yandex.ru>
2023-11-25 09:20:11 +0300

    👆 1685. Sum of Absolute Differences in a Sorted Array

--- 1685.sum-of-absolute-differences-in-a-sorted-array-c.c
+++ 1685.sum-of-absolute-differences-in-a-sorted-array-c.c
@@ -6,21 +6,18 @@
 int*
 getSumAbsoluteDifferences(int* nums, int nums_size, int* return_size)
 {
-  int prefix[100000], suffix[100000];
-  prefix[0] = nums[0];
-  for (int i = 1; i < nums_size; ++i) {
-    prefix[i] = prefix[i - 1] + nums[i];
-  }
-  suffix[nums_size - 1] = nums[nums_size - 1];
-  for (int i = nums_size - 1; i-- > 0;) {
-    suffix[i] = suffix[i + 1] + nums[i];
+  int sum = 0;
+  for (int i = 0; i < nums_size; ++i) {
+    sum += nums[i];
   }
 
   int* result = calloc(nums_size, sizeof *result);
   *return_size = nums_size;
 
+  int prefix_sum = 0;
   for (int i = 0; i < nums_size; ++i) {
-    result[i] = nums[i] * (i + 1) - prefix[i] + suffix[i] - nums[i] * (nums_size - i);
+    prefix_sum += nums[i];
+    result[i] = nums[i] * (2 * i + 2 - nums_size) - 2 * prefix_sum + sum;
   }
 
   return result;
