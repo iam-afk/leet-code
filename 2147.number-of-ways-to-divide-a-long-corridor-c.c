ea4ad798517938da853b581d4d4ee86add1e9826
Andrey Manzharov <amanzharov@yandex.ru>
2023-11-28 19:11:46 +0300

    ✅ 2147. Number of Ways to Divide a Long Corridor

---
+++ 2147.number-of-ways-to-divide-a-long-corridor-c.c
@@ -0,0 +1,30 @@
+int
+numberOfWays(char* corridor)
+{
+  const uint64_t M = 1e9 + 7;
+
+  int s = 0;
+  char* t = corridor;
+  uint64_t answer = 1;
+  while (true) {
+    while (s < 2 && *t != 0) {
+      if (*t == 'S')
+        ++s;
+      ++t;
+    }
+    if (s < 2)
+      return 0;
+    if (*t == 0)
+      return answer;
+    int p = 0;
+    while (*t != 0 && *t != 'S') {
+      ++p;
+      ++t;
+    }
+    if (*t == 0)
+      return answer;
+    answer = answer * (p + 1) % M;
+    s = 0;
+  }
+  return answer;
+}
