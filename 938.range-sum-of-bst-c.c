40e89fb52171e1ce7f00aeb709d86066acec3b07
Andrey Manzharov <amanzharov@yandex.ru>
2024-01-08 11:00:48 +0300

    ✅ 938. Range Sum of BST

---
+++ 938.range-sum-of-bst-c.c
@@ -0,0 +1,21 @@
+// @leet start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+int
+rangeSumBST(struct TreeNode* root, int low, int high)
+{
+  if (root == 0)
+    return 0;
+  if (root->val > high)
+    return rangeSumBST(root->left, low, high);
+  if (root->val < low)
+    return rangeSumBST(root->right, low, high);
+  return rangeSumBST(root->left, low, high) + root->val + rangeSumBST(root->right, low, high);
+}
+// @leet end
