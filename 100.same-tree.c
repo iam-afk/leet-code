81ce3f35be74664fa1e9b168fd332e8eb6a7d197
Andrey Manzharov <amanzharov@yandex.ru>
2024-02-26 08:13:06 +0300

    ✅ 100. Same Tree
    󰓅 Runtime
    
    	│ 2 ms
    	│ Beats 54.67% of users with  C
    
    󰍛 Memory
    
    	│ 5.7 MB
    	│ Beats 67.46% of users with  C

---
+++ 100.same-tree.c
@@ -0,0 +1,16 @@
+// @leet start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+bool
+isSameTree(struct TreeNode* p, struct TreeNode* q)
+{
+  return (p == 0 && q == 0) || (p != 0 && q != 0 && p->val == q->val &&
+                                isSameTree(p->left, q->left) && isSameTree(p->right, q->right));
+}
+// @leet end
