542c3440a8cf0cdd38a4aef2fd40cff6cec3ba90
Andrey Manzharov <amanzharov@yandex.ru>
2024-02-28 07:52:50 +0300

    ✅ 513. Find Bottom Left Tree Value
    󰓅 Runtime
    
    	│ 9 ms
    	│ Beats 15.58% of users with  C
    
    󰍛 Memory
    
    	│ 8.5 MB
    	│ Beats 81.82% of users with  C

---
+++ 513.find-bottom-left-tree-value.c
@@ -0,0 +1,34 @@
+// @leet start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+struct leftmost_at
+{
+  int val;
+  int depth;
+};
+
+static void
+update_leftmost_at_depth(struct TreeNode* node, struct leftmost_at* leftmost_at, int depth)
+{
+  if (node == 0)
+    return;
+  if (depth > leftmost_at->depth)
+    *leftmost_at = (struct leftmost_at){ .val = node->val, .depth = depth };
+  update_leftmost_at_depth(node->left, leftmost_at, depth + 1);
+  update_leftmost_at_depth(node->right, leftmost_at, depth + 1);
+}
+
+int
+findBottomLeftValue(struct TreeNode* root)
+{
+  struct leftmost_at leftmost_at = {};
+  update_leftmost_at_depth(root, &leftmost_at, 1);
+  return leftmost_at.val;
+}
+// @leet end
