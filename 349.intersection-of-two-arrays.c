0960a21e715f26d61d44453544b3b02a52a4019e
Andrey Manzharov <amanzharov@yandex.ru>
2024-03-10 08:36:18 +0300

    👆 349. Intersection of Two Arrays
    󰓅 Runtime
    
    	│ 0 ms
    	│ Beats 100.00% of users with  C
    
    󰍛 Memory
    
    	│ 5.9 MB
    	│ Beats 93.13% of users with  C

--- 349.intersection-of-two-arrays.c
+++ 349.intersection-of-two-arrays.c
@@ -15,16 +15,17 @@ intersection(int* nums1, int nums1_size, int* nums2, int nums2_size, int* ret_si
   qsort(nums2, nums2_size, sizeof(int), compare_ints);
 
   int buf[1000], size = 0;
-  if (bsearch(nums1, nums2, nums2_size, sizeof(int), compare_ints) != 0)
-    buf[size++] = nums1[0];
 
-  for (int i = 1; i < nums1_size; ++i) {
-    if (nums1[i - 1] == nums1[i])
-      continue;
-    if (bsearch(nums1 + i, nums2, nums2_size, sizeof(int), compare_ints) != 0)
+  for (int i = 0, j = 0; i < nums1_size && j < nums2_size;)
+    if (nums1[i] < nums2[j])
+      ++i;
+    else if (nums1[i] > nums2[j])
+      ++j;
+    else {
       buf[size++] = nums1[i];
-  }
-
+      while (i < nums1_size && nums1[i] == nums2[j])
+        ++i;
+    }
   *ret_size = size;
   int* ret = calloc(size, sizeof *ret);
   memcpy(ret, buf, size * sizeof *ret);
