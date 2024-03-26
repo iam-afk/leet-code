057af89a12140a611f04240274257c277e64e19e
Andrey Manzharov <amanzharov@yandex.ru>
2024-03-26 08:18:31 +0300

    👆 41. First Missing Positive
    󰓅 Runtime
    
    	│ 41 ms
    	│ Beats 58.76% of users with  C
    
    󰍛 Memory
    
    	│ 10.6 MB
    	│ Beats 91.63% of users with  C

--- 41.first-missing-positive.c
+++ 41.first-missing-positive.c
@@ -4,18 +4,16 @@ firstMissingPositive(int* nums, int nums_size)
 {
   int j = 0;
   for (int i = 0; i < nums_size;) {
-    if (1 <= nums[i] && nums[i] <= nums_size && nums[i] - 1 <= i) {
+    if (1 <= nums[i] && nums[i] <= nums_size && nums[i] != nums[nums[i] - 1]) {
       int t = nums[i];
       nums[i] = nums[nums[i] - 1];
       nums[t - 1] = t;
-      if (i == j || t - 1 == j)
-        while (j <= i && nums[j] == j + 1)
-          ++j;
-      if (t >= i + 1 || t == nums[i])
-        ++i;
     } else
       ++i;
   }
-  return j + 1;
+  for (int i = 0; i < nums_size; ++i)
+    if (nums[i] != i + 1)
+      return i + 1;
+  return nums_size + 1;
 }
 // @leet end
