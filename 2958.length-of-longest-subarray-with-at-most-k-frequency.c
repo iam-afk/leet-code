59c07c0e70ca2ce80786e15b4c1a530af229eb88
Andrey Manzharov <amanzharov@yandex.ru>
2024-03-28 12:51:12 +0300

    ✅ 2958. Length of Longest Subarray With at Most K Frequency
    󰓅 Runtime
    
    	│ 204 ms
    	│ Beats 36.36% of users with  C
    
    󰍛 Memory
    
    	│ 21.5 MB
    	│ Beats 63.64% of users with  C

---
+++ 2958.length-of-longest-subarray-with-at-most-k-frequency.c
@@ -0,0 +1,41 @@
+// @leet start
+#define max(a, b)                                                                                  \
+  ({                                                                                               \
+    __typeof__(a) _a = (a);                                                                        \
+    __typeof__(b) _b = (b);                                                                        \
+    _a > _b ? _a : _b;                                                                             \
+  })
+
+int
+maxSubarrayLength(int* nums, int nums_size, int k)
+{
+  struct
+  {
+    int value, number;
+    UT_hash_handle hh;
+  } *hash = 0, *entry, *ptr, buffer[100000], *next = buffer;
+
+  int answer = 0;
+
+  int left = 0, right = 0;
+  for (int left = 0, right = 0; right < nums_size; ++right) {
+    HASH_FIND_INT(hash, &nums[right], entry);
+    if (entry)
+      ++entry->number;
+    else {
+      entry = next++;
+      entry->value = nums[right];
+      entry->number = 1;
+      HASH_ADD_INT(hash, value, entry);
+    }
+    while (entry->number > k) {
+      HASH_FIND_INT(hash, &nums[left], ptr);
+      --ptr->number;
+      ++left;
+    }
+    answer = max(answer, right - left + 1);
+  }
+
+  return answer;
+}
+// @leet end
