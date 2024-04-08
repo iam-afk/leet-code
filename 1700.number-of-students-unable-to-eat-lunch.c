a52a4711186a1927df58e3d37bd610c23ca23368
Andrey Manzharov <amanzharov@yandex.ru>
2024-04-08 08:05:46 +0300

    ✅ 1700. Number of Students Unable to Eat Lunch
    󰓅 Runtime
    
    	│ 0 ms
    	│ Beats 100.00% of users with  C
    
    󰍛 Memory
    
    	│ 6 MB
    	│ Beats 20.29% of users with  C

---
+++ 1700.number-of-students-unable-to-eat-lunch.c
@@ -0,0 +1,23 @@
+// @leet start
+int
+countStudents(int* students, int students_size, int* sandwiches, int sandwiches_size)
+{
+  int c = 0, s = 0;
+  for (int i = 0; i < students_size; ++i)
+    if (students[i] == 0)
+      ++c;
+    else
+      ++s;
+  for (int i = 0; i < sandwiches_size; ++i)
+    if (sandwiches[i] == 0) {
+      if (c == 0)
+        return s;
+      --c;
+    } else {
+      if (s == 0)
+        return c;
+      --s;
+    }
+  return 0;
+}
+// @leet end
