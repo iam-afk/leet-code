8ea5f000406c28d96d75f30b066fd38207227afb
Andrey Manzharov <amanzharov@yandex.ru>
2024-04-10 09:53:48 +0300

    👆 950. Reveal Cards In Increasing Order
    󰓅 Runtime
    
    	│ 12 ms
    	│ Beats 38.46% of users with  C
    
    󰍛 Memory
    
    	│ 6.9 MB
    	│ Beats 69.23% of users with  C

--- 950.reveal-cards-in-increasing-order.c
+++ 950.reveal-cards-in-increasing-order.c
@@ -2,6 +2,7 @@
 /**
  * Note: The returned array must be malloced, assume caller calls free().
  */
+#include <string.h>
 static int
 compare_ints(void const* a, void const* b)
 {
@@ -14,26 +15,19 @@ deckRevealedIncreasing(int* deck, int deck_size, int* ret_size)
   *ret_size = deck_size;
   int* ret = calloc(*ret_size, sizeof *ret);
 
+  int q[UINT16_MAX];
+  uint16_t h = 0, t = deck_size;
+  for (int i = 0; i < deck_size; ++i)
+    q[i] = i;
+
   qsort(deck, deck_size, sizeof(int), compare_ints);
 
-  int order[1000], q[1000];
-  for (int i = 0; i < deck_size; ++i)
-    order[i] = i;
-  int n = deck_size, t = 0, k = 0, i = 0;
-  while (n > 0) {
-    if (i < n)
-      q[k++] = order[i++];
-    else {
-      n = t, t = 0, i = 0;
-      continue;
-    }
-    if (i < n)
-      order[t++] = order[i++];
-    else
-      n = t, t = 1, i = 1;
+  for (int i = 0; i < deck_size; ++i) {
+    int x = q[h++];
+    ret[x] = deck[i];
+    q[t++] = q[h++];
   }
-  for (int i = 0; i < deck_size; ++i)
-    ret[q[i]] = deck[i];
+
   return ret;
 }
 // @leet end
