0db8aef3bb19f87bcff6dfc89f84c25a66f89a77
Andrey Manzharov <amanzharov@yandex.ru>
2024-07-30 16:44:05 +0300

    ✅ 1653. Minimum Deletions to Make String Balanced
    󰓅 Runtime
    
    	│ 56 ms
    	│ Beats 33.33% of users with  C
    
    󰍛 Memory
    
    	│ 12.8 MB
    	│ Beats 33.33% of users with  C

---
+++ 1653.minimum-deletions-to-make-string-balanced.c
@@ -0,0 +1,28 @@
+// @leet start
+#define min(a, b)                                                                                  \
+  ({                                                                                               \
+    __typeof__(a) _a = (a);                                                                        \
+    __typeof__(b) _b = (b);                                                                        \
+    _a < _b ? _a : _b;                                                                             \
+  })
+
+int d[(size_t)1e5 + 1];
+
+int
+minimumDeletions(char* s)
+{
+  int n = d[n] = 0;
+  for (int b = 0; s[n]; ++n) {
+    if (s[n] == 'b')
+      ++b;
+    d[n + 1] = b;
+  }
+  int mn = d[n];
+  for (int i = n, a = 0; i-- > 0;) {
+    if (s[i] == 'a')
+      ++a;
+    mn = min(mn, a + d[i]);
+  }
+  return mn;
+}
+// @leet end
