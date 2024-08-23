f0f2c30b44f12fe84c19c187dbe35a5235c7c617
Andrey Manzharov <amanzharov@yandex.ru>
2024-08-23 13:57:36 +0300

    ✅ 396. Rotate Function
    󰓅 Runtime
    
    	│ 101 ms
    	│ Beats 94.12% of users with  C
    
    󰍛 Memory
    
    	│ 15.8 MB
    	│ Beats 29.41% of users with  C

---
+++ 396.rotate-function.c
@@ -0,0 +1,24 @@
+// @leet start
+#define max(a, b)                                                                                  \
+  ({                                                                                               \
+    __typeof__(a) _a = (a);                                                                        \
+    __typeof__(b) _b = (b);                                                                        \
+    _a > _b ? _a : _b;                                                                             \
+  })
+
+int
+maxRotateFunction(int* nums, int numsSize)
+{
+  int sum = 0, f = 0;
+  for (int i = 0; i < numsSize; ++i) {
+    sum += nums[i];
+    f += i * nums[i];
+  }
+  int mx = f;
+  for (int i = numsSize; i-- > 0;) {
+    f += sum - nums[i] * numsSize;
+    mx = max(mx, f);
+  }
+  return mx;
+}
+// @leet end
