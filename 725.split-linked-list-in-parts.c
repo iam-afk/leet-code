21e332de9376386268eee14292377769e02b3ff3
Andrey Manzharov <amanzharov@yandex.ru>
2024-09-10 14:04:36 +0300

    ✅ 725. Split Linked List in Parts
    󰓅 Runtime
    
    	│ 3 ms
    	│ Beats 76.41% of users with  C
    
    󰍛 Memory
    
    	│ 11 MB
    	│ Beats 36.21% of users with  C

---
+++ 725.split-linked-list-in-parts.c
@@ -0,0 +1,35 @@
+// @leet start
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+struct ListNode**
+splitListToParts(struct ListNode* head, int k, int* returnSize)
+{
+  struct ListNode** ret = calloc(k, sizeof(struct ListNode*));
+  *returnSize = k;
+
+  for (int i = 0; i < k; ++i) {
+    struct ListNode *slow = head, *fast = head;
+    while (fast) {
+      for (int j = i; fast && j < k; ++j) {
+        fast = fast->next;
+      }
+      struct ListNode* last = slow;
+      slow = slow->next;
+      if (!fast)
+        last->next = 0;
+    }
+    ret[i] = head;
+    head = slow;
+  }
+
+  return ret;
+}
+// @leet end
