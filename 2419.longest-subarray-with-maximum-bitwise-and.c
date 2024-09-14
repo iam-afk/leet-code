f29ec618d415d0495ec448b72d0dd0b474d4d116
Andrey Manzharov <amanzharov@yandex.ru>
2024-09-14 06:45:20 +0300

    ✅ 2419. Longest Subarray With Maximum Bitwise AND
    󰓅 Runtime
    
    	│ 98 ms
    	│ Beats 71.43% of users with  C
    
    󰍛 Memory
    
    	│ 16.4 MB
    	│ Beats 21.43% of users with  C

---
+++ 2419.longest-subarray-with-maximum-bitwise-and.c
@@ -0,0 +1,25 @@
+// @leet start
+#define max(a, b)                                                                                  \
+  ({                                                                                               \
+    __typeof__(a) _a = (a);                                                                        \
+    __typeof__(b) _b = (b);                                                                        \
+    _a > _b ? _a : _b;                                                                             \
+  })
+
+int
+longestSubarray(int* nums, int numsSize)
+{
+  int mx = 0;
+  for (int i = 0; i < numsSize; ++i)
+    mx = max(mx, nums[i]);
+  int ans = 0;
+  for (int i = 0, d = 0; i < numsSize; ++i) {
+    if (nums[i] != mx)
+      d = 0;
+    else
+      ++d;
+    ans = max(ans, d);
+  }
+  return ans;
+}
+// @leet end
