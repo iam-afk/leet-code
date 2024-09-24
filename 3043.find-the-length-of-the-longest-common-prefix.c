d0cb45e3aa3af9447a38a22e44dcd849765dfeb0
Andrey Manzharov <amanzharov@yandex.ru>
2024-09-24 15:35:28 +0300

    👆 3043. Find the Length of the Longest Common Prefix
    󰓅 Runtime
    
    	│ 237 ms
    	│ Beats 20.00% of users with  C
    
    󰍛 Memory
    
    	│ 52.2 MB
    	│ Beats 100.00% of users with  C

--- 3043.find-the-length-of-the-longest-common-prefix.c
+++ 3043.find-the-length-of-the-longest-common-prefix.c
@@ -1,37 +1,40 @@
 // @leet start
+
+struct node
+{
+  struct node* next[11];
+};
+
 int
 longestCommonPrefix(int* arr1, int arr1Size, int* arr2, int arr2Size)
 {
-  struct
-  {
-    int num, len;
-    UT_hash_handle hh;
-  } *head = 0, buffer[9 * (size_t)5e4], *ptr, *next = buffer;
+  struct node buffer[9 * 50000], *next = buffer;
+  struct node root = { .next = {} };
 
+  char buf[10];
   for (int i = 0; i < arr1Size; ++i) {
-    for (int n = arr1[i]; n > 0; n /= 10) {
-      HASH_FIND_INT(head, &n, ptr);
-      if (ptr)
-        break;
-      ptr = next++;
-      ptr->num = n;
-      HASH_ADD_INT(head, num, ptr);
+    sprintf(buf, "%d", arr1[i]);
+    struct node* p = &root;
+    for (int j = 0; buf[j]; ++j) {
+      int d = buf[j] - '0';
+      if (!p->next[d]) {
+        p->next[d] = next++;
+        memset(p->next[d]->next, 0, sizeof(struct node));
+      }
+      p = p->next[d];
     }
   }
 
   int mx = 0;
   for (int i = 0; i < arr2Size; ++i) {
-    int n = arr2[i];
-    for (int n = arr2[i]; n > 0; n /= 10) {
-      HASH_FIND_INT(head, &n, ptr);
-      if (ptr) {
-        int l = 0, p = 1;
-        while (p <= n)
-          p *= 10, ++l;
-        if (mx < l)
-          mx = l;
-      }
+    sprintf(buf, "%d", arr2[i]);
+    struct node* p = &root;
+    int j = 0;
+    for (; buf[j] && p->next[buf[j] - '0']; ++j) {
+      p = p->next[buf[j] - '0'];
     }
+    if (mx < j)
+      mx = j;
   }
 
   return mx;
