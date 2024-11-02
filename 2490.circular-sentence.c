33aa7c8fbf008668b652b3c63d20c11d807a3c9b
Andrey Manzharov <amanzharov@yandex.ru>
2024-11-02 07:51:03 +0300

    👆 2490. Circular Sentence
    󰓅 Runtime
    
    	│ 0 ms
    	│ Beats 100.00% of users with  C
    
    󰍛 Memory
    
    	│ 8.1 MB
    	│ Beats 16.67% of users with  C

--- 2490.circular-sentence.c
+++ 2490.circular-sentence.c
@@ -3,16 +3,10 @@ bool
 isCircularSentence(char* sentence)
 {
   char last = 0;
-  int i = 0;
-  while (sentence[i]) {
-    while (sentence[i] && sentence[i] != ' ')
-      last = sentence[i++];
-    if (sentence[i] == 0)
-      break;
-    while (sentence[i] == ' ')
-      ++i;
-    if (sentence[i] && sentence[i] != last)
+  for (int i = 0; sentence[i]; ++i) {
+    if (sentence[i] == ' ' && sentence[i - 1] != sentence[i + 1])
       return false;
+    last = sentence[i];
   }
   return *sentence == last;
 }
