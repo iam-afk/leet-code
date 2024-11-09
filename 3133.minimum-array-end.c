0a8e9637721bb39cfee84b9a10c700b1a6450f58
Andrey Manzharov <amanzharov@yandex.ru>
2024-11-09 10:07:31 +0300

    👆 3133. Minimum Array End
    󰓅 Runtime
    
    	│ 0 ms
    	│ Beats 100.00% of users with  C
    
    󰍛 Memory
    
    	│ 8.3 MB
    	│ Beats 33.33% of users with  C

--- 3133.minimum-array-end.c
+++ 3133.minimum-array-end.c
@@ -2,9 +2,12 @@
 long long
 minEnd(int n, int x)
 {
-  long long y = x;
-  for (int i = 1; i < n; ++i)
-    y = (y + 1) | x;
-  return y;
+  long long y = x, m = n - 1, i = 0;
+  for (; x > 0; ++i, x >>= 1)
+    if ((x & 1) == 0) {
+      y |= (m & 1) << i;
+      m >>= 1;
+    }
+  return y | m << i;
 }
 // @leet end
