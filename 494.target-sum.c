df011437030c0f06bc795cd0ab972aeeaaf8c7b3
Andrey Manzharov <amanzharov@yandex.ru>
2024-12-26 08:32:06 +0300

    ✅ 494. Target Sum
    󰓅 Runtime
    
    	│ 3 ms
    	│ Beats 80.30% of users with  C
    
    󰍛 Memory
    
    	│ 9.2 MB
    	│ Beats 28.79% of users with  C

---
+++ 494.target-sum.c
@@ -0,0 +1,30 @@
+// @leet start
+#define N 1000
+
+#define swap(a, b)                                                                                 \
+  {                                                                                                \
+    __typeof__(a) _t = (a);                                                                        \
+    (a) = (b);                                                                                     \
+    (b) = _t;                                                                                      \
+  }
+
+int
+findTargetSumWays(int* nums, int numsSize, int target)
+{
+  int d[2][2 * N + 1] = {}, *p = d[0], *q = d[1];
+  p[N] = 1;
+  int sum = 0;
+  for (int i = 0; i < numsSize; ++i) {
+    memset(q, 0, (2 * N + 1) * sizeof *q);
+    for (int x = -sum; x <= sum; ++x) {
+      if (p[N + x] > 0) {
+        q[N + x - nums[i]] += p[N + x];
+        q[N + x + nums[i]] += p[N + x];
+      }
+    }
+    swap(p, q);
+    sum += nums[i];
+  }
+  return p[N + target];
+}
+// @leet end
