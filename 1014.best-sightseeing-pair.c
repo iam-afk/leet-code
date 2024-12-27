3f7658e23d7e21db49e091ce5160cf1441f1702d
Andrey Manzharov <amanzharov@yandex.ru>
2024-12-27 08:29:54 +0300

    ✅ 1014. Best Sightseeing Pair
    󰓅 Runtime
    
    	│ 3 ms
    	│ Beats 12.50% of users with  C
    
    󰍛 Memory
    
    	│ 11.3 MB
    	│ Beats 50.00% of users with  C

---
+++ 1014.best-sightseeing-pair.c
@@ -0,0 +1,22 @@
+// @leet start
+
+#define max(a, b)                                                                                  \
+  ({                                                                                               \
+    __typeof__(a) _a = (a);                                                                        \
+    __typeof__(b) _b = (b);                                                                        \
+    _a > _b ? _a : _b;                                                                             \
+  })
+
+int
+maxScoreSightseeingPair(int* values, int valuesSize)
+{
+  int i = 0;
+  int ans = 0;
+  for (int j = 1; j < valuesSize; ++j) {
+    ans = max(ans, values[j] + values[i] + i - j);
+    if (values[i] - values[j] <= j - i)
+      i = j;
+  }
+  return ans;
+}
+// @leet end
