412f49a1db78acd8923e1ddf7b5002b976472f2e
Andrey Manzharov <amanzharov@yandex.ru>
2025-02-12 09:26:44 +0300

    2342. Max Sum of a Pair With Equal Sum of Digits
    󰓅 Runtime
    
    	│ 9 ms
    	│ Beats 22.22% of users with  C
    
    󰍛 Memory
    
    	│ 16 MB
    	│ Beats 11.11% of users with  C

---
+++ 2342.max-sum-of-a-pair-with-equal-sum-of-digits.c
@@ -0,0 +1,24 @@
+// @leet start
+#define max(a, b)                                                                                  \
+  ({                                                                                               \
+    __typeof__(a) _a = (a);                                                                        \
+    __typeof__(b) _b = (b);                                                                        \
+    _a > _b ? _a : _b;                                                                             \
+  })
+
+int
+maximumSum(int* nums, int numsSize)
+{
+  int sums[100] = {};
+  int ans = -1;
+  for (int i = 0; i < numsSize; ++i) {
+    int s = 0;
+    for (int n = nums[i]; n > 0; n /= 10)
+      s += n % 10;
+    if (sums[s] > 0)
+      ans = max(ans, nums[i] + sums[s]);
+    sums[s] = max(sums[s], nums[i]);
+  }
+  return ans;
+}
+// @leet end
