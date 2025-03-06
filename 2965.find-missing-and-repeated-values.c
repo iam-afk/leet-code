49ce618328b09a354b9dc450936cf18cc51761c0
Andrey Manzharov <amanzharov@yandex.ru>
2025-03-06 09:25:56 +0300

    👆 2965. Find Missing and Repeated Values
    󰓅 Runtime
    
    	│ 0 ms
    	│ Beats 100.00% of users with  C
    
    󰍛 Memory
    
    	│ 11.3 MB
    	│ Beats 100.00% of users with  C

--- 2965.find-missing-and-repeated-values.c
+++ 2965.find-missing-and-repeated-values.c
@@ -3,25 +3,23 @@
  * Note: The returned array must be malloced, assume caller calls free().
  */
 int*
-findMissingAndRepeatedValues(int** grid, int gridSize, int* gridColSize, int* retSize)
+findMissingAndRepeatedValues(int** g, int n, int* gridColSize, int* retSize)
 {
-  int* ret = calloc(2, sizeof *ret);
-  *retSize = 2;
+  int64_t perfect_sum = n * n * (n * n + 1) / 2;
+  int64_t perfect_sum_2 = 1LL * n * n * (n * n + 1) * (2 * n * n + 1) / 6;
 
-  bool f[50 * 50] = {};
-
-  for (int i = 0; i < gridSize; ++i)
-    for (int j = 0; j < gridSize; ++j) {
-      int n = grid[i][j];
-      if (f[n - 1])
-        ret[0] = n;
-      else
-        f[n - 1] = true;
+  int64_t sum = 0, sum_2 = 0;
+  for (int i = 0; i < n; ++i)
+    for (int j = 0; j < n; ++j) {
+      sum += g[i][j];
+      sum_2 += g[i][j] * g[i][j];
     }
-  for (int i = 1; i <= gridSize * gridSize; ++i)
-    if (!f[i - 1])
-      ret[1] = i;
 
+  *retSize = 2;
+  int* ret = calloc(2, sizeof *ret);
+  ret[0] = (sum_2 - perfect_sum_2) / (sum - perfect_sum) + sum - perfect_sum;
+  ret[0] /= 2;
+  ret[1] = ret[0] - sum + perfect_sum;
   return ret;
 }
 // @leet end
