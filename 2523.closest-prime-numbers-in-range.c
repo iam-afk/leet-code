05f525b7c4778fa15d07203f253b6c092a2821cd
Andrey Manzharov <amanzharov@yandex.ru>
2025-03-07 08:10:02 +0300

    ✅ 2523. Closest Prime Numbers in Range
    󰓅 Runtime
    
    	│ 149 ms
    	│ Beats 25.00% of users with  C
    
    󰍛 Memory
    
    	│ 8.8 MB
    	│ Beats 100.00% of users with  C

---
+++ 2523.closest-prime-numbers-in-range.c
@@ -0,0 +1,37 @@
+// @leet start
+/**
+ * Note: The returned array must be malloced, assume caller calls free().
+ */
+int*
+closestPrimes(int left, int right, int* returnSize)
+{
+  bool p[1000010];
+  memset(p, true, right + 1);
+  p[1] = false;
+  for (int i = 2; i <= right; ++i)
+    if (p[i])
+      for (uint64_t j = 1LL * i * i; j <= right; j += i)
+        p[j] = false;
+
+  *returnSize = 2;
+  int* ret = calloc(2, sizeof *ret);
+  ret[0] = ret[1] = -1;
+
+  int num1 = left;
+  for (; num1 <= right; ++num1)
+    if (p[num1])
+      break;
+  if (num1 > right)
+    return ret;
+
+  int num2 = num1 + 1;
+  for (; num2 <= right; ++num2)
+    if (p[num2]) {
+      if (ret[0] < 0 || num2 - num1 < ret[1] - ret[0])
+        ret[0] = num1, ret[1] = num2;
+      num1 = num2;
+    }
+
+  return ret;
+}
+// @leet end
