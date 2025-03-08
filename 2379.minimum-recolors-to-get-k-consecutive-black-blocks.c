1e62c734324db38e9ae1cddec972e6fb942e9763
Andrey Manzharov <amanzharov@yandex.ru>
2025-03-08 18:20:39 +0300

    2379. Minimum Recolors to Get K Consecutive Black Blocks
    󰓅 Runtime
    
    	│ 0 ms
    	│ Beats 100.00% of users with  C
    
    󰍛 Memory
    
    	│ 8 MB
    	│ Beats 47.06% of users with  C

---
+++ 2379.minimum-recolors-to-get-k-consecutive-black-blocks.c
@@ -0,0 +1,23 @@
+// @leet start
+#define min(a, b)                                                                                  \
+  ({                                                                                               \
+    __typeof__(a) _a = (a);                                                                        \
+    __typeof__(b) _b = (b);                                                                        \
+    _a < _b ? _a : _b;                                                                             \
+  })
+
+int
+minimumRecolors(char* blocks, int k)
+{
+  int d[101];
+  d[0] = 0;
+  int ans = 10000;
+  for (int i = 0; blocks[i]; ++i) {
+    d[i + 1] = d[i] + (blocks[i] == 'W');
+    if (i + 1 >= k)
+      ans = min(ans, d[i + 1] - d[i - k + 1]);
+  }
+
+  return ans;
+}
+// @leet end
