142f6377908663e54858a92a7b680444e5594588
Andrey Manzharov <amanzharov@yandex.ru>
2025-03-12 16:13:52 +0300

    👆 1358. Number of Substrings Containing All Three Characters
    󰓅 Runtime
    
    	│ 11 ms
    	│ Beats 37.50% of users with  C
    
    󰍛 Memory
    
    	│ 8.7 MB
    	│ Beats 37.50% of users with  C

--- 1358.number-of-substrings-containing-all-three-characters.c
+++ 1358.number-of-substrings-containing-all-three-characters.c
@@ -4,26 +4,12 @@ numberOfSubstrings(char* s)
 {
   int ans = 0;
   int n = strlen(s);
-  int a, b, c;
-  a = b = c = 0;
+  int d[3] = {};
   for (int i = 0, j = 0; i < n; ++i) {
-    if (s[i] == 'a')
-      ++a;
-    if (s[i] == 'b')
-      ++b;
-    if (s[i] == 'c')
-      ++c;
-    if (a > 0 && b > 0 && c > 0) {
-      int k;
-      for (k = j; a > 0 && b > 0 && c > 0; ++j) {
-        if (s[j] == 'a')
-          --a;
-        if (s[j] == 'b')
-          --b;
-        if (s[j] == 'c')
-          --c;
-      }
-      ans += (n - i) * (j - k);
+    ++d[s[i] - 'a'];
+    while ( d[0] > 0 && d[1] > 0 && d[2] > 0) {
+      ans += n - i;
+      --d[s[j++] - 'a'];
     }
   }
   return ans;
