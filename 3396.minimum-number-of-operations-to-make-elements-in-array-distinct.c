f77e368ff75b98a015eebf18616a2f7153ce137e
Andrey Manzharov <amanzharov@yandex.ru>
2025-04-08 08:48:51 +0300

    ✅ 3396. Minimum Number of Operations to Make Elements in Array Distinct
    󰓅 Runtime
    
    	│ 0 ms
    	│ Beats 100.00% of users with  C
    
    󰍛 Memory
    
    	│ 9.6 MB
    	│ Beats 18.06% of users with  C

---
+++ 3396.minimum-number-of-operations-to-make-elements-in-array-distinct.c
@@ -0,0 +1,22 @@
+// @leet start
+#define max(a, b)                                                                                  \
+  ({                                                                                               \
+    __typeof__(a) _a = (a);                                                                        \
+    __typeof__(b) _b = (b);                                                                        \
+    _a > _b ? _a : _b;                                                                             \
+  })
+
+int
+minimumOperations(int* nums, int numsSize)
+{
+  int d[101] = { [0 ... 100] = -1 };
+  int mx = -1;
+  for (int i = 0; i < numsSize; ++i) {
+    if (d[nums[i]] >= 0)
+      mx = max(mx, d[nums[i]]);
+    d[nums[i]] = i;
+  }
+  printf("%d\n", mx);
+  return mx < 0 ? 0 : mx / 3 + 1;
+}
+// @leet end
