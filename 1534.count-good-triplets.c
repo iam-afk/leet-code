7e5598953ab58332c8a057e1e0d96b605ef0c2fc
Andrey Manzharov <amanzharov@yandex.ru>
2025-04-14 09:05:31 +0300

    ✅ 1534. Count Good Triplets
    󰓅 Runtime
    
    	│ 0 ms
    	│ Beats 100.00% of users with  C
    
    󰍛 Memory
    
    	│ 8.1 MB
    	│ Beats 93.98% of users with  C

---
+++ 1534.count-good-triplets.c
@@ -0,0 +1,14 @@
+// @leet start
+int
+countGoodTriplets(int* arr, int arrSize, int a, int b, int c)
+{
+  int ans = 0;
+  for (int i = 0; i < arrSize; ++i)
+    for (int j = i + 1; j < arrSize; ++j)
+      if (abs(arr[i] - arr[j]) <= a)
+        for (int k = j + 1; k < arrSize; ++k)
+          if (abs(arr[j] - arr[k]) <= b && abs(arr[i] - arr[k]) <= c)
+            ++ans;
+  return ans;
+}
+// @leet end
