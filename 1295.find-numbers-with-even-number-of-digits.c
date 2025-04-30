d5d330274ca350531ceff8f36363f185a45bb1f7
Andrey Manzharov <amanzharov@yandex.ru>
2025-04-30 07:44:48 +0300

    ✅ 1295. Find Numbers with Even Number of Digits
    󰓅 Runtime
    
    	│ 0 ms
    	│ Beats 100.00% of users with  C
    
    󰍛 Memory
    
    	│ 8.3 MB
    	│ Beats 46.82% of users with  C

---
+++ 1295.find-numbers-with-even-number-of-digits.c
@@ -0,0 +1,11 @@
+// @leet start
+int
+findNumbers(int* nums, int numsSize)
+{
+  int ans = 0;
+  for (int i = 0; i < numsSize; ++i)
+    if (10 <= nums[i] && nums[i] < 100 || 1000 <= nums[i] && nums[i] < 10000 || 100000 <= nums[i])
+      ++ans;
+  return ans;
+}
+// @leet end
