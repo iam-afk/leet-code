1ef4333dcab3cb5bb88c9c109d79ce3be3a52d00
Andrey Manzharov <amanzharov@yandex.ru>
2025-05-03 09:07:51 +0300

    ✅ 1007. Minimum Domino Rotations For Equal Row
    󰓅 Runtime
    
    	│ 0 ms
    	│ Beats 100.00% of users with  C
    
    󰍛 Memory
    
    	│ 13.6 MB
    	│ Beats 100.00% of users with  C

---
+++ 1007.minimum-domino-rotations-for-equal-row.c
@@ -0,0 +1,34 @@
+// @leet start
+#define min(a, b)                                                                                  \
+  ({                                                                                               \
+    __typeof__(a) _a = (a);                                                                        \
+    __typeof__(b) _b = (b);                                                                        \
+    _a < _b ? _a : _b;                                                                             \
+  })
+#define max(a, b)                                                                                  \
+  ({                                                                                               \
+    __typeof__(a) _a = (a);                                                                        \
+    __typeof__(b) _b = (b);                                                                        \
+    _a > _b ? _a : _b;                                                                             \
+  })
+
+int
+minDominoRotations(int* tops, int topsSize, int* bottoms, int bottomsSize)
+{
+  int n = topsSize;
+  int top[7] = {}, bottom[7] = {}, equal[7] = {};
+  for (int i = 0; i < n; ++i) {
+    ++top[tops[i]];
+    ++bottom[bottoms[i]];
+    if (tops[i] == bottoms[i])
+      ++equal[tops[i]];
+  }
+  int ans = INT_MAX;
+  for (int i = 1; i <= 6; ++i) {
+    if (top[i] + bottom[i] - equal[i] >= n) {
+      ans = min(ans, n - max(top[i], bottom[i]));
+    }
+  }
+  return ans < INT_MAX ? ans : -1;
+}
+// @leet end
