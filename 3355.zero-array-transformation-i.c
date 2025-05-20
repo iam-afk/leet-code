0ee2ece7ade065f5f82dce2b83a67c4fd99e712a
Andrey Manzharov <amanzharov@yandex.ru>
2025-05-20 09:16:50 +0300

    ✅ 3355. Zero Array Transformation I
    󰓅 Runtime
    
    	│ 4 ms
    	│ Beats 75.00% of users with  C
    
    󰍛 Memory
    
    	│ 58.4 MB
    	│ Beats 91.67% of users with  C

---
+++ 3355.zero-array-transformation-i.c
@@ -0,0 +1,22 @@
+// @leet start
+
+bool
+isZeroArray(int* nums, int numsSize, int** queries, int queriesSize, int* queriesColSize)
+{
+  int d[100001];
+  memset(d, 0, numsSize * sizeof *d);
+  for (int i = 0; i < queriesSize; ++i) {
+    int l = queries[i][0], r = queries[i][1];
+    ++d[l], --d[r + 1];
+  }
+
+  int s = 0;
+  for (int i = 0; i < numsSize; ++i) {
+    s += d[i];
+    if (s < nums[i])
+      return false;
+  }
+
+  return true;
+}
+// @leet end
