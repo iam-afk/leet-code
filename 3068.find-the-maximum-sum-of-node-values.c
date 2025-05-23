da8d49082bfeaba51fb68f41fec8ec3e934d46b9
Andrey Manzharov <amanzharov@yandex.ru>
2025-05-23 08:42:24 +0300

    👆 3068. Find the Maximum Sum of Node Values
    󰓅 Runtime
    
    	│ 0 ms
    	│ Beats 100.00% of users with  C
    
    󰍛 Memory
    
    	│ 25.1 MB
    	│ Beats 100.00% of users with  C

---
+++ 3068.find-the-maximum-sum-of-node-values.c
@@ -0,0 +1,34 @@
+// @leet start
+#define min(a, b)                                                                                  \
+  ({                                                                                               \
+    __typeof__(a) _a = (a);                                                                        \
+    __typeof__(b) _b = (b);                                                                        \
+    _a < _b ? _a : _b;                                                                             \
+  })
+#define max(a, b)                                                                                  \
+  ({                                                                                               \
+    __typeof__(a) _a = (a);                                                                        \
+    __typeof__(b) _b = (b);                                                                        \
+    _a > _b ? _a : _b;                                                                             \
+  })
+
+long long
+maximumValueSum(int* nums, int numsSize, int k, int** edges, int edgesSize, int* edgesColSize)
+{
+  int64_t sum = 0, pmn = INT_MAX, nmx = INT_MIN, n = 0;
+  for (int i = 0; i < numsSize; ++i) {
+    sum += nums[i];
+    int d = (nums[i] ^ k) - nums[i];
+    if (d > 0) {
+      sum += d;
+      pmn = min(pmn, d);
+      ++n;
+    } else {
+      nmx = max(nmx, d);
+    }
+  }
+  if (n & 1)
+    return max(sum + nmx, sum - pmn);
+  return sum;
+}
+// @leet end
