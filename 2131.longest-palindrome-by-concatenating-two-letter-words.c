224cf6d3a203fdfa88a87f46d51f1a5cade51493
Andrey Manzharov <amanzharov@yandex.ru>
2025-05-25 07:48:31 +0300

    👆 2131. Longest Palindrome by Concatenating Two Letter Words
    󰓅 Runtime
    
    	│ 0 ms
    	│ Beats 100.00% of users with  C
    
    󰍛 Memory
    
    	│ 52.2 MB
    	│ Beats 28.57% of users with  C

--- 2131.longest-palindrome-by-concatenating-two-letter-words.c
+++ 2131.longest-palindrome-by-concatenating-two-letter-words.c
@@ -9,22 +9,22 @@
 int
 longestPalindrome(char** words, int wordsSize)
 {
-  int d[26 * 26] = {};
+  int d[26][26] = {};
   for (int i = 0; i < wordsSize; ++i) {
     int a = words[i][0] - 'a', b = words[i][1] - 'a';
-    ++d[a * 26 + b];
+    ++d[a][b];
   }
-  int ans = 0;
-  bool c = false;
-  for (int i = 0; i < 26 * 26; ++i) {
-    int a = i / 26, b = i % 26;
-    if (a != b)
-      ans += min(d[i], d[b * 26 + a]) * 2;
-    else {
-      ans += d[i] / 2 * 4;
-      c = c || d[i] & 1;
+  int ans = 0, c = 0;
+  for (int i = 0; i < 26; ++i) {
+    for (int j = i; j < 26; ++j) {
+      if (i != j)
+        ans += min(d[i][j], d[j][i]);
+      else {
+        ans += d[i][j] / 2;
+        c = c || d[i][j] & 1;
+      }
     }
   }
-  return ans + c * 2;
+  return ans * 4 + c * 2;
 }
 // @leet end
