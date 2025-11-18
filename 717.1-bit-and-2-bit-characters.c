d91b64020582e7676050ee6d4693430d76cea8f8
Andrey Manzharov <amanzharov@yandex.ru>
2025-11-18 07:01:11 +0300

    ✅ 717. 1-bit and 2-bit Characters
    󰓅 Runtime
    
    	│ 0 ms
    	│ Beats 100.00% of users with  C
    
    󰍛 Memory
    
    	│ 8.3 MB
    	│ Beats 25.44% of users with  C

---
+++ 717.1-bit-and-2-bit-characters.c
@@ -0,0 +1,13 @@
+// @leet start
+bool
+isOneBitCharacter(int* bits, int bitsSize)
+{
+  int i = 0;
+  while (i < bitsSize - 1)
+    if (bits[i] == 1)
+      i += 2;
+    else
+      i += 1;
+  return i < bitsSize;
+}
+// @leet end
