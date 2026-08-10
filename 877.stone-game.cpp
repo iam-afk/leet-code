05612a1c1a9f9b57a2119d1ea425d967184e3791
Andrey Manzharov <amanzharov@yandex.ru>
2026-08-10 13:25:41 +0300

    ✅ 877. Stone Game
    󰓅 Runtime
    
    	│ 0 ms
    	│ Beats 100.00% of users with  C++
    
    󰍛 Memory
    
    	│ 10.3 MB
    	│ Beats 85.83% of users with  C++

---
+++ 877.stone-game.cpp
@@ -0,0 +1,11 @@
+// @leet imports start
+using namespace std;
+// @leet imports end
+
+// @leet start
+class Solution
+{
+public:
+  bool stoneGame(vector<int>& piles) { return true; }
+};
+// @leet end
